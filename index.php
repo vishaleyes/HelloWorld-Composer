3a184d4401c62b397c53bda571d928abdf5effe2
Vishal Panchal <vishal.panchal@bypt.in>
2017-08-17 20:07:02 +0530

    Committed the first composer package

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
+
+use HelloWorld\SayHello;
+
+echo SayHello::world();
+
+?>
---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vishal
+ * Date: 17-08-2017
+ * Time: 17:29
+ */
+
+namespace HelloWorld;
+
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
